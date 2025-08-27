f5a47efdd7f20d6b4e58d073b7855c5f3671e0b2
W1ndys <w1ndys@outlook.com>
2025-08-27 20:24:48 +0800

    feat(course-plan): 修改公告模态框，增加链接点击功能并优化样式以提升用户体验

--- frontend/pages/course-plan/NoticeModal.vue
+++ frontend/pages/course-plan/NoticeModal.vue
@@ -14,9 +14,10 @@
                     <text class="notice-text" style="margin-top: 24rpx">
                         根据教务处官方规定：培养方案毕业学分原则上145-170学分，其中人文社会科学类专业毕业学分≤160学分，理科类专业毕业学分≤165学分，工科类专业毕业学分≤170学分，辅修学士学位专业毕业学分70学分左右。
                     </text>
-                    <text class="notice-text" style="margin-top: 16rpx; font-size: 24rpx; color: #8c8c8c">
-                        来源：https://jwc.qfnu.edu.cn/info/1068/7039.htm
-                    </text>
+                    <view style="margin-top: 16rpx;">
+                        <text class="notice-text" style="font-size: 24rpx; color: #8c8c8c">来源：</text>
+                        <text class="link-text" @click="openLink">https://jwc.qfnu.edu.cn/info/1068/7039.htm</text>
+                    </view>
                 </view>
 
                 <view class="modal-actions">
@@ -45,8 +46,46 @@ const handleClose = () => {
 const handleConfirm = () => {
     emit('confirm');
 };
+
+const openLink = () => {
+    // #ifdef H5
+    window.open('https://jwc.qfnu.edu.cn/info/1068/7039.htm', '_blank');
+    // #endif
+
+    // #ifdef MP-WEIXIN
+    uni.setClipboardData({
+        data: 'https://jwc.qfnu.edu.cn/info/1068/7039.htm',
+        success: () => {
+            uni.showToast({
+                title: '链接已复制到剪贴板',
+                icon: 'success'
+            });
+        }
+    });
+    // #endif
+
+    // #ifdef APP-PLUS
+    plus.runtime.openURL('https://jwc.qfnu.edu.cn/info/1068/7039.htm');
+    // #endif
+};
 </script>
 
 <style lang="scss" scoped>
 @import './NoticeModal.scss';
+
+.link-text {
+    font-size: 24rpx;
+    color: #1890ff;
+    text-decoration: underline;
+    cursor: pointer;
+    transition: color 0.3s ease;
+}
+
+.link-text:hover {
+    color: #40a9ff;
+}
+
+.link-text:active {
+    color: #096dd9;
+}
 </style>
